99a0c107c87b823af5f28a763a64673951bf5bbe
davidabutler92 <davidabutler92@yahoo.com>
2020-08-19 19:24:19 -0700

    add document ready function w/ submit for form-pig-latin, store vowels array in variable and consonants array in a variable

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#piglatin-survey").ready(function(event) {
+    event.preventDefault();
+
+    const userInput = $("#userInput").val();
+    const vowels = ["a", "e", "i", "o", "u"];
+    const consonants = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
+    let pigLatin = [];
+  });
+});
