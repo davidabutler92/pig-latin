39428cb4ae460de02650a2a93860e39f7554ab2c
davidabutler92 <davidabutler92@yahoo.com>
2020-08-19 19:49:56 -0700

    add function that only accepts leters and white spaces

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  $("form#piglatin-survey").ready(function(event) {
+  $("#piglatin-survey").submit(function(event) {
     event.preventDefault();
 
     const userInput = $("#userInput").val();
+    console.log(userInput);
     const vowels = ["a", "e", "i", "o", "u"];
     const consonants = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
     let pigLatin = [];
+
+    function isLetter(userInput) { 
+      if (!isNaN(userInput)) {
+        alert("Letters only! No numbers please, enter a phrase.");
+      }
+    }
+    isLetter(userInput);
   });
 });
