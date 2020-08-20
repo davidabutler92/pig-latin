1826dc08b1ac8c733f9cae5301fd387749999ab2
davidabutler92 <davidabutler92@yahoo.com>
2020-08-19 20:27:06 -0700

    add else if statement for words that start with qu, push qu to end then add ay

--- js/main.js
+++ js/main.js
@@ -3,25 +3,31 @@ $(document).ready(function() {
     event.preventDefault();
 
     const userInput = $("#userInput").val();
-    // console.log(userInput);
     const vowels = ["a", "e", "i", "o", "u"];
     const consonants = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
-    let pigLatin = [];
+    let pigLatinArr = [];
 
+    //Function that only accepts letters and white spaces
     function isLetter(userInput) { 
       if (!isNaN(userInput)) {
         alert("Letters only! No numbers please, enter a phrase.");
       }
     }
     isLetter(userInput); //<<< I learned that you have to call the funtion to use it.
-
+    
+    //funtion for translating userInput and returning pig latin
     const splitArr = userInput.split(" ");
-    // console.log(splitArr);
     splitArr.forEach(word => {
       if(vowels.includes(word[0])) {
         word = word + "way"
-        pigLatin.push(word);
-      } else if ()
+        pigLatinArr.push(word);
+      } else if (word[0] === "q" && word[1] === "u") {
+        word = word + word[0] + word[1] + "ay"
+        word = word.substring(2, word.length-1)
+        pigLatinArr.push(word);
+      }
+      
     });
+    console.log(pigLatinArr);
   });
 });
