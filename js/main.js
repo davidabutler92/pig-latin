d77fb1e6e64fd737753d5d4c9a900bfdae7d439a
davidabutler92 <davidabutler92@yahoo.com>
2020-08-19 20:09:09 -0700

    add a forEach loop to loop through user input with an if statement looking for words that start with vowels, then add way to the end with .push

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     const userInput = $("#userInput").val();
-    console.log(userInput);
+    // console.log(userInput);
     const vowels = ["a", "e", "i", "o", "u"];
     const consonants = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
     let pigLatin = [];
@@ -16,6 +16,12 @@ $(document).ready(function() {
     isLetter(userInput); //<<< I learned that you have to call the funtion to use it.
 
     const splitArr = userInput.split(" ");
-    console.log(splitArr);
+    // console.log(splitArr);
+    splitArr.forEach(word => {
+      if(vowels.includes(word[0])) {
+        word = word + "way"
+        pigLatin.push(word);
+      } else if ()
+    });
   });
 });
